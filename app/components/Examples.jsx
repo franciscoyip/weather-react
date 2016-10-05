2ea0be335f3f850df38f747207724b80e1836211
Francisco Yip <francisco.yip@gmail.com>
2016-10-04 17:23:54 -0700

    added style to navbar

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = function(props){
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Example locations to play around</p>
+      <ol>
+        <li><Link to="/?location=Fremont, CA">Fremont, CA</Link></li>
+        <li><Link to="/?location=San Francisco, CA">San Francisco, CA</Link></li>
+        <li><Link to="/?location=Rio, Brazil">Rio, Brazil</Link></li>
+      </ol>
+    </div>
+
   );
 };
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,11 +3,15 @@ var {Link, IndexLink} = require('react-router');
 
 var Navbar = function(props){
   return (
-    <div>
-      <h3>Navbar</h3>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+    <div className='top-bar'>
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">Weather</li>
+          <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+          <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+          <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+        </ul>
+      </div>
     </div>
   );
 };
